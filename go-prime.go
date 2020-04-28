09be19669eb0db0a7e76a47f50523dc6637f34de
Don Togbe <albert.togbe@gmail.com>
2020-04-28 06:37:06 +0000

    Added timeout and refactored printing
    Printing results from the main thread only

--- go-prime.go
+++ go-prime.go
@@ -4,62 +4,73 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"runtime"
 	"strconv"
+	"time"
 )
 
 /*Num represents a number type that may be Prime.*/
 type Num struct {
-	number  float64
+	number  int64
 	isPrime bool
 }
 
 func main() {
-	num1, _ := strconv.ParseFloat(os.Args[1], 64)
-	num2, _ := strconv.ParseFloat(os.Args[2], 64)
+	num1, _ := strconv.ParseInt(os.Args[1], 10, 64)
+	num2, _ := strconv.ParseInt(os.Args[2], 10, 64)
+	var numCPU = runtime.GOMAXPROCS(0)
+	fmt.Println("Using", numCPU, "cores")
 	var chanMax int64
-	var c chan bool
+	var c = make(chan *Num, numCPU)
+	timeout := time.After(5 * time.Second)
 
 	fmt.Println("Finding prime numbers between", num1, "and ", num2)
 	if num2 > num1 {
-		chanMax = int64(num2 - num1)
-		c = make(chan bool, chanMax)
+		chanMax = num2 - num1
 		for num := num1; num <= num2; num++ {
 			go checkIfPrime(num, c)
-
 		}
+		fmt.Println("Goroutines fired!!!")
 	} else {
-		chanMax = int64(num1 - num2)
-		c = make(chan bool, chanMax)
+		chanMax = num1 - num2
 		for num := num2; num <= num1; num++ {
 			go checkIfPrime(num, c)
 		}
+		fmt.Println("Goroutines fired!!!")
 	}
 	for i := 0; i < int(chanMax); i++ {
 		select {
-		case <-c:
+		case n := <-c:
+			if n.isPrime {
+				fmt.Println(n.number, "is prime")
+			}
+		case <-timeout:
+			continue
 		}
 	}
 }
 
-func checkIfPrime(num float64, c chan bool) {
+func checkIfPrime(num int64, c chan *Num) {
+	n := &Num{number: num, isPrime: false}
 	if num <= 1 {
-		c <- false
+		c <- n
+		return
 	}
 	if num == 2 || num == 3 {
-		fmt.Println(num, "is prime")
-		c <- true
-	} else if int64(num)%2 == 0 {
-		c <- false
+		n.isPrime = true
+		c <- n
+	} else if num%2 == 0 {
+		c <- n
 	} else {
 		limit := math.Sqrt(float64(num))
-		for n := float64(3); n <= limit; n += 2 {
-			if int64(num)%int64(n) == 0 {
-				c <- false
+		for i := int64(3); i <= int64(limit); i += 2 {
+			if num%i == 0 {
+				c <- n
 				return
 			}
 		}
-		fmt.Println(num, "is prime")
-		c <- true
+		n.isPrime = true
+		c <- n
 	}
 
 }
