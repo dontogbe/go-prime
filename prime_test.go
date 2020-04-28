79e1d6da597a18db788ff08d74fc403341e88129
Don Togbe <albert.togbe@gmail.com>
2020-04-28 07:38:37 +0000

    Added test

---
+++ prime_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPrime(t *testing.T) {
+	c := make(chan *Num)
+	go checkIfPrime(3, c)
+	num := <-c
+	if num.isPrime != true {
+		t.Error("Was expecting true")
+	}
+	fmt.Println(num.number)
+}
